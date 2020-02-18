d36ae56424b583fb9713717d29c6f7ff6374abd3
Nelson Moller <nmoller.c@gmail.com>
2020-02-17 19:57:52 -0500

    Refactor to use message and messagehandlers.

---
+++ src/Message/AddPonkaToImage.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Message;
+
+
+use App\Entity\ImagePost;
+
+class AddPonkaToImage
+{
+
+    private $imagePost;
+
+    public function __construct(ImagePost $imagePost)
+    {
+        $this->imagePost = $imagePost;
+    }
+
+    public function getImagePost(): ImagePost
+    {
+        return $this->imagePost;
+    }
+}
